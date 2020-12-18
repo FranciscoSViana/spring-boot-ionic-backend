52d8689842608fe444b326a1fbe8cf3003e19fda
Francisco Viana <franciscosantos29@gmail.com>
2020-12-18 14:33:26 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/fsv/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.fsv.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fsv.cursomc.domain.Cliente;
+import com.fsv.cursomc.repositories.ClienteRepository;
+import com.fsv.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
