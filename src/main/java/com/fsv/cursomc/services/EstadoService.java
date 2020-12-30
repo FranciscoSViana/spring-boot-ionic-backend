4e5a2cfb3daa182f72bdccdfd9493f143f858416
Francisco Viana <franciscosantos29@gmail.com>
2020-12-30 16:10:53 -0300

    Endpoint para buscar estados e cidades

---
+++ src/main/java/com/fsv/cursomc/services/EstadoService.java
@@ -0,0 +1,20 @@
+package com.fsv.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fsv.cursomc.domain.Estado;
+import com.fsv.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repo;
+
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+}
